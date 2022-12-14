1479934fe978786c6aa47bb63b171b4bf0ee4dd6
brianwambuamu <mutisobrian210@gmail.com>
2022-12-14 21:58:38 +0300

    updating js

--- index.js
+++ index.js
@@ -74,3 +74,19 @@ function getStars(rating) {
     return output.join('');
   
   }
+
+  //first call to display random movies
+const getMovies=() =>{
+    fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1")
+    .then(response => response.json())
+    .then(data =>{ 
+        data.results.forEach(element => {
+            createElements(element)
+        });
+        setTimeout(() => {
+            document.getElementById('lds-roller').style = "display:none";
+        }, 10000);
+    
+    });
+
+}
