ace16367afe3712b8c0385a1cb5fe700b8beef59
brianwambuamu <mutisobrian210@gmail.com>
2022-12-14 21:57:26 +0300

    updating js

--- index.js
+++ index.js
@@ -49,3 +49,28 @@ const createElements = (x) => {
   row_body.appendChild(col);
   
 }
+
+
+// document.getElementById("stars").innerHTML = getStars(3.6);
+
+function getStars(rating) {
+
+    // Round to nearest half
+    rating = Math.round(rating * 2) / 2;
+    new_rating = (rating/10)*5;
+    let output = [];
+  
+    // Append all the filled whole stars
+    for (var i = new_rating; i >= 1; i--)
+      output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+  
+    // If there is a half a star, append it
+    if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+  
+    // Fill the empty stars
+    for (let i = (5 - new_rating); i >= 1; i--)
+      output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+  
+    return output.join('');
+  
+  }
